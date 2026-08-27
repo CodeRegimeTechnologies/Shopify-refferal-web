64168ce468771cf6f9b33020aac6403292cf7a66
Yogeshwaran <coderegime@gmail.com>
2026-08-27 13:21:35 +0530

    Fix blank screen: correctly set basename and 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function AppRoutes() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <AppRoutes />
         <Toaster position="top-right" toastOptions={{
